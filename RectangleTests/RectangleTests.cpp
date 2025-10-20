ff1423a2d8d570c1dfa31dbc4024c02567dd9cf7
Afsana Zaman <Azaman6489@conestogac.on.ca>
2025-10-19 22:05:45 -0400

    Add initial unit tests for setLength and setWidth.

--- RectangleTests/RectangleTests.cpp
+++ RectangleTests/RectangleTests.cpp
@@ -30,5 +30,48 @@ namespace RectangleTests
 			Assert::AreEqual(30, result);  //2 * (10 + 5) = 30
 		}
 
+		//Tests for setLength
+		TEST_METHOD(Test_SetLength_ValidValue)
+		{
+			int length = 1;
+			setLength(10, &length);
+			Assert::AreEqual(10, length);
+		}
+
+		TEST_METHOD(Test_SetLength_MinValue)
+		{
+			int length = 5;
+			setLength(1, &length);  //edge case: minimum allowed
+			Assert::AreEqual(1, length);
+		}
+
+		TEST_METHOD(Test_SetLength_InvalidValue)
+		{
+			int length = 5;
+			setLength(150, &length); //invalid, should not change
+			Assert::AreEqual(150, length);
+		}
+
+		//Tests for setWidth
+		TEST_METHOD(Test_SetWidth_ValidValue)
+		{
+			int width = 1;
+			setWidth(20, &width);
+			Assert::AreEqual(20, width);
+		}
+
+		TEST_METHOD(Test_SetWidth_MinValue)
+		{
+			int width = 10;
+			setWidth(1, &width);
+			Assert::AreEqual(1, width);
+		}
+
+		TEST_METHOD(Test_SetWidth_InvalidValue)
+		{
+			int width = 5;
+			setWidth(0, &width);
+			Assert::AreEqual(0, width);
+		}
 	};
 }
